c78e41eeb9156187c165a84960a4470cfa716d49
natalimaevskaya <54096402+NataliMaevskaya@users.noreply.github.com>
2020-02-07 17:56:05 +0300

    changes

---
+++ main.js
@@ -0,0 +1,22 @@
+let mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.arrow',
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 2,
+          spaceBetween: 40
+        }
+    }
+});
+
+const menuButton = document.querySelector ('.menu-button'),
+    menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', function () {
+    menuButton.classList.toggle('menu-button-active');
+    menu.classList.toggle('header-active');
+});
+
